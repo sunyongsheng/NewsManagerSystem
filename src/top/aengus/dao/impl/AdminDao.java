96f12a57907dd5788285210a4910f774704db9d4
wxb <3249381309@qq.com>
2020-01-09 15:52:32 +0800

    完成管理员功能

--- src/top/aengus/dao/impl/AdminDao.java
+++ src/top/aengus/dao/impl/AdminDao.java
@@ -1,6 +1,14 @@
 package top.aengus.dao.impl;
 
 import top.aengus.dao.interfaces.AdminInterface;
+import top.aengus.pojo.Admin;
+import top.aengus.pojo.Author;
+import top.aengus.utils.DBUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * @author Aengus Sun
@@ -9,14 +17,81 @@ import top.aengus.dao.interfaces.AdminInterface;
  * @since 2020/1/9
  */
 public class AdminDao implements AdminInterface {
+    Connection connection;
+    PreparedStatement preparedStatement;
+    ResultSet resultSet;
+
+    @Override
+    public boolean adminLogin(Admin admin) {
+        try {
+            connection = DBUtil.getConnection();
+            String sql = "Select * from administrator u where u.admin_id=? and u.admin_password=?";
+            assert connection != null;
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, admin.getAdminId());
+            preparedStatement.setString(2, admin.getAdminPassword());
+            resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                return true;
+            } else return false;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 
     @Override
     public boolean deleteAuthorByAuthorId(String authorId) {
+        boolean flag = false;
+        try {
+            connection = DBUtil.getConnection();
+            String sql = "Delete from author where author_id =?";
+            assert connection != null;
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, authorId);
+
+            if (preparedStatement.executeUpdate() > 0) {
+                flag = true;
+            }
+            return flag;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DBUtil.close(connection, preparedStatement, resultSet);
+        }
+
+
         return false;
     }
 
     @Override
     public boolean deleteNewsByNewsId(int newsId) {
+        boolean flag = false;
+        try {
+            connection = DBUtil.getConnection();
+            String sql = "Delete from news where news_id =?";
+            assert connection != null;
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, newsId);
+
+            if (preparedStatement.executeUpdate() > 0) {
+                flag = true;
+            }
+            return flag;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DBUtil.close(connection, preparedStatement, resultSet);
+        }
+
+
         return false;
+    }
+
+    public static void main(String[] args) {
+        AdminDao adminDao = new AdminDao();
+        Admin admin = new Admin("admin", "1234");
+        System.out.println(adminDao.deleteNewsByNewsId(1));
     }
 }
