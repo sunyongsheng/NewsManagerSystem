a275b8c3e6866b87935e2427da09a1d4108f54dd
Aengus Sun <sunyongsheng6511@gmail.com>
2020-01-10 11:29:44 +0800

    新增是否ID存在Servlet

---
+++ src/top/aengus/service/ExistIdServlet.java
@@ -0,0 +1,35 @@
+package top.aengus.service;
+
+import top.aengus.dao.impl.AuthorDao;
+import top.aengus.dao.interfaces.AuthorInterface;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Aengus Sun
+ * Email: sys6511@126.com | sunyongsheng6511@gmail.com
+ * @link https://www.aengus.top
+ * @since 2020/1/10
+ */
+@WebServlet("/existId")
+public class ExistIdServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        response.setCharacterEncoding("utf-8");
+        String authorId = request.getParameter("author_id");
+        AuthorInterface authorInterface = new AuthorDao();
+        if (!authorInterface.authorIdAvailable(authorId)) {
+            response.getWriter().print("用户名已被占用");
+        } else {
+            response.getWriter().print("用户名可用");
+        }
+    }
+}
