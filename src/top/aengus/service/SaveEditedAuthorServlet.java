00cf4a8225412a74f9b9dd40c33a8ce0cabf1b37
Aengus Sun <sunyongsheng6511@gmail.com>
2020-01-10 20:16:53 +0800

    修改增加作者后的跳转逻辑

--- src/top/aengus/service/SaveEditedAuthorServlet.java
+++ src/top/aengus/service/SaveEditedAuthorServlet.java
@@ -1,7 +1,7 @@
 package top.aengus.service;
 
-import top.aengus.dao.impl.AdminDao;
-import top.aengus.dao.interfaces.AdminInterface;
+import top.aengus.dao.impl.AuthorDao;
+import top.aengus.dao.interfaces.AuthorInterface;
 import top.aengus.pojo.Author;
 
 import javax.servlet.ServletException;
@@ -17,24 +17,21 @@ import java.io.IOException;
  * @link https://www.aengus.top
  * @since 2020/1/10
  */
-@WebServlet("/addAuthor")
-public class AddAuthorServlet extends HttpServlet {
+@WebServlet("/saveAuthor")
+public class SaveEditedAuthorServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
         String authorId = request.getParameter("author_id");
         String authorPassword = request.getParameter("author_password");
         String authorName = request.getParameter("author_name");
-        Author author = new Author(authorId, authorPassword, authorName);
-        AdminInterface adminInterface = new AdminDao();
-        if (adminInterface.addAuthor(author)) {
-            request.setAttribute("addAuthorMessage", "添加作者成功");
+        AuthorInterface authorInterface = new AuthorDao();
+        if (authorInterface.authorUpdate(authorId, new Author(authorId, authorPassword, authorName))) {
+            request.setAttribute("addAuthorMessage", "更新信息成功");
         } else {
-            request.setAttribute("addAuthorMessage", "添加作者失败");
+            request.setAttribute("addAuthorMessage", "更新信息失败");
         }
-        if (request.getSession().getAttribute("user").toString().equals("admin")) {
-            request.getRequestDispatcher("getAllNews").forward(request, response);
-        } else {
-            request.getRequestDispatcher("login.jsp").forward(request, response);
-        }
+        request.getRequestDispatcher("getAllNews").forward(request, response);
+
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
