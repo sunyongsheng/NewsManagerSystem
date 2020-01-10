dca162a3ac85c792b5ca9f8fddeddfb029253173
Aengus Sun <sunyongsheng6511@gmail.com>
2020-01-10 14:02:34 +0800

    新增 搜索新闻Servlet

---
+++ src/top/aengus/service/SearchNewsByTitleServlet.java
@@ -0,0 +1,34 @@
+package top.aengus.service;
+
+import top.aengus.dao.impl.NewsDao;
+import top.aengus.dao.interfaces.NewsInterface;
+import top.aengus.pojo.News;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @author Aengus Sun
+ * Email: sys6511@126.com | sunyongsheng6511@gmail.com
+ * @link https://www.aengus.top
+ * @since 2020/1/10
+ */
+@WebServlet("/searchNewsByTitle")
+public class SearchNewsByTitleServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String queryText = request.getParameter("search_title");
+        NewsInterface newsInterface = new NewsDao();
+        List<News> searchRes = newsInterface.getNewsByNewsTitle(queryText);
+        request.setAttribute("search_result", searchRes);
+        request.getRequestDispatcher("index.jsp").forward(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+}
