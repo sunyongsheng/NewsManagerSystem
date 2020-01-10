19397a3f34ce4e947b7c32e5b8f53a3e5676b53a
Aengus Sun <sunyongsheng6511@gmail.com>
2020-01-10 14:26:41 +0800

    新增 通过分类获取新闻Servlet

---
+++ src/top/aengus/service/GetNewsByCategoryServlet.java
@@ -0,0 +1,34 @@
+package top.aengus.service;
+
+import top.aengus.dao.impl.NewsDao;
+import top.aengus.dao.interfaces.NewsInterface;
+import top.aengus.pojo.News;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @author Aengus Sun
+ * Email: sys6511@126.com | sunyongsheng6511@gmail.com
+ * @link https://www.aengus.top
+ * @since 2020/1/10
+ */
+@WebServlet("/getNewsByCategory")
+public class GetNewsByCategoryServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+        String category = request.getParameter("category");
+        NewsInterface newsInterface = new NewsDao();
+        List<News> res = newsInterface.getNewsByCategory(category);
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+}
