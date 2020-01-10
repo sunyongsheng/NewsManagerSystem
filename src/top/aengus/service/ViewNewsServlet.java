3edfc4c6339ecbf6990cb4207038747d210a5d6a
Aengus Sun <sunyongsheng6511@gmail.com>
2020-01-10 17:03:48 +0800

    新增查看新闻Servlet

---
+++ src/top/aengus/service/ViewNewsServlet.java
@@ -0,0 +1,34 @@
+package top.aengus.service;
+
+import top.aengus.dao.impl.NewsDao;
+import top.aengus.dao.interfaces.NewsInterface;
+import top.aengus.pojo.News;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Aengus Sun
+ * Email: sys6511@126.com | sunyongsheng6511@gmail.com
+ * @link https://www.aengus.top
+ * @since 2020/1/10
+ */
+@WebServlet("/viewNews")
+public class ViewNewsServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+        int newsId = Integer.parseInt(request.getParameter("news_id"));
+        NewsInterface newsInterface = new NewsDao();
+        News news = newsInterface.getNewsByNewsId(newsId);
+        request.setAttribute("news", news);
+        request.getRequestDispatcher("view-news.jsp").forward(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+}
